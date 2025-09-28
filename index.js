1abf42c68d5dac81e0bbaeb4c3b7e1dfea68291a
jswebdevs <jskeya116@gmail.com>
2025-09-28 15:00:17 +0600

    Uploading Backend

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://rajpropertyfront.netlify.app/", "https://www.rajproperty.site"], // allow frontend URLs
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
